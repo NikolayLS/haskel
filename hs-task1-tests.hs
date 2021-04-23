4888e02d811c8aff0ebfc4dac185ff5f59f77e29
Nikolay Stefanov <64357665+NikolayLS@users.noreply.github.com>
2021-04-23 13:10:58 +0300

    Update hs-task1-tests.hs

--- hs-task1-tests.hs
+++ hs-task1-tests.hs
@@ -1,37 +1,14 @@
-{--
-  СУ "Св. Климент Охридски"
-  Факултет по математика и информатика
-  Курс Функционално програмиране 2020/21
-  Контролно 3
-  2021-01-16
-
-  Име:Николай Любомиров Стефанов
-  ФН:45545
-  Специалност:Информатика
-  Курс:3
-  Административна група:1
-  Начален час на контролното: 9;45
---}
-
 
 module K3_45545_Test where
 
 import Test.HUnit
 import K3_45545  
 
--- Даденият по-долу пример е само ориентировъчен.
--- Когато решавате задачите си, можете да го изтриете
--- или да го промените така, че да проверява условия
--- свързани с вашия код.
 
--- Може да използвате пълния синтаксис...
-
---тестове по първа задача
 task1Test1 = TestCase $ assertEqual "sample check" True (isStringinTree "abd" sampleTree)
 task1Test2 = TestCase $ assertEqual  "sample check" True (isStringinTree "ab" sampleTree)
 task1Test3 = TestCase $ assertEqual  "sample check" True (isStringinTree "a" sampleTree)
 
---тестове по втора задача
 task2Test1 = TestCase $ assertEqual  "sample check" True (isInjective succ 5 100)
 task2Test2 = TestCase $ assertEqual  "sample check" True (isInjective sample 5 100)
 task2Test3 = TestCase $ assertEqual  "sample check" False (isInjective even 5 100)
